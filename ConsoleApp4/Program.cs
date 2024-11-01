e8fa37180b7eb28e2eb5236715b707221a909fb7
PC <PC@DESKTOP-8HGPVBP>
2024-11-01 16:30:36 -0400

    Agregar archivos de proyecto.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class EmailSender
+{
+    public void EnviarCorreo(string destinatario, string mensaje)
+    {
+       
+    }
+}
+public class Persona
+{
+    private string nombre;
+    private int edad;
+    private string direccion;
+    private string correoElectronico;
+
+    public void ImprimirDatos()
+    {
+        Console.WriteLine($"Nombre: {nombre}");
+        Console.WriteLine($"Edad: {edad}");
+        Console.WriteLine($"Dirección: {direccion}");
+        Console.WriteLine($"Correo electrónico: {correoElectronico}");
+    }
+
+    public void EnviarCorreo(string mensaje)
+    {
+    
+        EmailSender emailSender = new EmailSender();
+        emailSender.EnviarCorreo(correoElectronico, mensaje);
+    }
+}
